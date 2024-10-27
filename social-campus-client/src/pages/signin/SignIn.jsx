117b3b11eb3ff33b238f79dec255975044ffcfcb
Oleksii Palamarchuk <144825066+PalamarchukOleksii@users.noreply.github.com>
2024-10-27 03:59:01 +0200

    add sign in page

---
+++ social-campus-client/src/pages/signin/SignIn.jsx
@@ -0,0 +1,39 @@
+// SignIn.js
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./SignIn.css";
+
+function SignIn() {
+  const navigate = useNavigate();
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    try {
+      // Handle sign-in logic here
+      console.log("Sign in logic goes here");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="signin">
+      <div className="signin-option">
+        <h1 className="general-text">Sign In</h1>
+        <form onSubmit={handleSignIn}>
+          <input type="text" placeholder="Email" required />
+          <input type="password" placeholder="Password" required />
+          <button type="submit" className="signin-button">
+            Sign In
+          </button>
+        </form>
+        <h2 className="not-general-text">Don&apos;t have an account?</h2>
+        <button className="signup-button" onClick={() => navigate("/signup")}>
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
