d61392625dcc98c6857b79a1f82644c2ba7b57ec
Oleksii Palamarchuk <144825066+PalamarchukOleksii@users.noreply.github.com>
2024-10-27 03:58:23 +0200

    change global styles for more contrast

--- social-campus-client/src/App.js
+++ social-campus-client/src/App.js
@@ -2,19 +2,21 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Signup from "./pages/Signup";
+import SignUp from "./pages/signup/SignUp";
 import Search from "./pages/Search";
 import Profile from "./pages/Profile";
 import Messanger from "./pages/Messanger";
-import Login from "./pages/Login";
+import SignIn from "./pages/signin/SignIn";
+import Welcom from "./pages/welcom/Welcom";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path={"/"} element={<Home />} />
-        <Route path={"/login"} element={<Login />} />
-        <Route path={"/signup"} element={<Signup />} />
+        <Route exact path={"/"} element={<Welcom />} />
+        <Route path={"/home"} element={<Home />} />
+        <Route path={"/signin"} element={<SignIn />} />
+        <Route path={"/signup"} element={<SignUp />} />
         <Route path={"/search"} element={<Search />} />
         <Route path={"/profile"} element={<Profile />} />
         <Route path={"/messanger"} element={<Messanger />} />
