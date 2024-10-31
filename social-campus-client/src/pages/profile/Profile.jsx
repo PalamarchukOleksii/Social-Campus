a50369fa97a7f7bf81d3c4d7f88ae5622c4c72f1
Oleksii Palamarchuk <144825066+PalamarchukOleksii@users.noreply.github.com>
2024-10-31 16:21:09 +0200

    remove anesesary coment

--- social-campus-client/src/pages/profile/Profile.jsx
+++ social-campus-client/src/pages/profile/Profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { Link } from "react-router-dom";
 import "./Profile.css";
 import userData from "../../data/userData.json";
 
