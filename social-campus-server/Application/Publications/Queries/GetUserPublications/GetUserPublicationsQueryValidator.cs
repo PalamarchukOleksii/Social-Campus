1f6480c1456d98fe107ae2bb888483779e881eea
Oleksii Palamarchuk <oleksiypalamarchuck@gmail.com>
2025-06-03 22:12:53 +0300

    add endpoint to get all users publications

---
+++ social-campus-server/Application/Publications/Queries/GetUserPublications/GetUserPublicationsQueryValidator.cs
@@ -0,0 +1,13 @@
+using FluentValidation;
+
+namespace Application.Publications.Queries.GetUserPublications;
+
+public class GetUserPublicationsQueryValidator : AbstractValidator<GetUserPublicationsQuery>
+{
+    public GetUserPublicationsQueryValidator()
+    {
+        RuleFor(f => f.UserId)
+            .NotEmpty().WithMessage("UserId is required")
+            .Must(id => id.Value != Guid.Empty).WithMessage("UserId must be a valid GUID");
+    }
+}
