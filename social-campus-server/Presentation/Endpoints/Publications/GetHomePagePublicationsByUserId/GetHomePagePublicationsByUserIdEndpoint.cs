59ad7ad6b12f9187888db0bdc13a20b3a17d42e7
Oleksii Palamarchuk <oleksiypalamarchuck@gmail.com>
2025-06-15 18:37:44 +0300

    feat: add GetHomePagePublicationsByUserIdEndpoint for retrieving user-specific publications on the homepage

---
+++ social-campus-server/Presentation/Endpoints/Publications/GetHomePagePublicationsByUserId/GetHomePagePublicationsByUserIdEndpoint.cs
@@ -0,0 +1,32 @@
+using Application.Publications.Queries.GetHomePagePublications;
+using Domain.Models.PublicationModel;
+using Domain.Models.UserModel;
+using MediatR;
+using Presentation.Abstractions;
+using Presentation.Consts;
+
+namespace Presentation.Endpoints.Publications.GetHomePagePublicationsByUserId;
+
+public class GetHomePagePublicationsByUserIdEndpoint : BaseEndpoint, IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("publications/home/{userId:guid}/{limit:int}", async (
+                Guid userId,
+                int limit,
+                Guid? lastPublicationId,
+                ISender sender) =>
+            {
+                var query = new GetHomePagePublicationsQuery(
+                    new UserId(userId),
+                    lastPublicationId is not null ? new PublicationId(lastPublicationId.Value) : null,
+                    limit);
+
+                var response = await sender.Send(query);
+
+                return response.IsSuccess ? Results.Ok(response.Value) : HandleFailure(response);
+            })
+            .WithTags(Tags.Publications)
+            .RequireAuthorization();
+    }
+}
