47fd14a85a7d2d39021f4cf2ff6aef5e9f6f4973
Oleksii Palamarchuk <oleksiypalamarchuck@gmail.com>
2025-06-26 15:52:22 +0300

    feat: implement IHasher interface and Hasher class for password hashing and verification

--- social-campus-server/Application/Abstractions/Security/IPasswordHasher.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Application.Abstractions.Security;
-
-public interface IPasswordHasher
-{
-    Task<string> HashAsync(string password);
-    Task<bool> VerifyAsync(string password, string passwordHash);
-}
