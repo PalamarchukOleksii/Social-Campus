f73a4476c3afb014ab2282ce59f74159185dfbf3
Oleksii Palamarchuk <oleksiypalamarchuck@gmail.com>
2025-06-26 15:51:44 +0300

    feat: consolidate email link factory interfaces and implement EmailLinkFactory class

---
+++ social-campus-server/Infrastructure/Email/EmailLinkFactory.cs
@@ -0,0 +1,35 @@
+using Application.Abstractions.Email;
+using Domain.Consts;
+using Domain.Models.EmailVerificationTokenModel;
+using Domain.Models.ResetPasswordTokenModel;
+using Domain.Models.UserModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Infrastructure.Email;
+
+public class EmailLinkFactory(IHttpContextAccessor httpContextAccessor, LinkGenerator linkGenerator)
+    : IEmailLinkFactory
+{
+    public string? CreateEmailVerificationLink(EmailVerificationTokenId emailVerificationTokenId)
+    {
+        var verificationLink =
+            linkGenerator.GetUriByName(
+                httpContextAccessor.HttpContext!,
+                LinkConsts.VerifyEmail,
+                new { token = emailVerificationTokenId.Value });
+
+        return verificationLink;
+    }
+
+    public string? CreateResetPasswordLink(ResetPasswordTokenId resetPasswordTokenId, UserId userId)
+    {
+        var resetLink =
+            linkGenerator.GetUriByName(
+                httpContextAccessor.HttpContext!,
+                LinkConsts.ResetPassword,
+                new { token = resetPasswordTokenId.Value, userId = userId.Value });
+
+        return resetLink;
+    }
+}
